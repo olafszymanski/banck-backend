543b44e2044e643be2df30f85a8c63727cbba395
Olaf Szymański <olaf.szym123@gmail.com>
2021-05-11 00:57:56 +0200

    Implemented refresh tokens.

--- banck/auth/routes.py
+++ banck/auth/routes.py
@@ -1,9 +1,9 @@
-import bcrypt
-from flask import Blueprint, request
+import bcrypt, jwt
+from flask import Blueprint, request, current_app
 from .schemas import login_schema
 from banck.users.models import User
 from banck.users.schemas import user_creation_schema
-from banck.utils import create_error, generate_token
+from banck.utils import create_error, generate_token, generate_refresh_token, decode_token
 
 
 auth = Blueprint('auth', __name__)
@@ -18,8 +18,13 @@ def login():
   password = request.json.get('password')
 
   if user := User.query.filter_by(email=email).first():
+    print(password.encode('utf8'))
+    print(user.password.encode('utf8'))
     if bcrypt.checkpw(password.encode('utf8'), user.password.encode('utf8')):
-      return { 'token': generate_token(user) }
+      return {
+        'token': generate_token(user),
+        'refresh_token': generate_refresh_token(user)
+      }
 
     return create_error('Incorrect password!', 403)
 
@@ -42,4 +47,29 @@ def signup():
 
   user.add()
 
-  return { 'token': generate_token(user) }
+  return {
+    'token': generate_token(user),
+    'refresh_token': generate_refresh_token(user)
+  }
+
+
+@auth.route('/auth/refresh_token', methods=['GET'])
+def refresh_token():
+  if token := request.args.get('token'):
+    try:
+      decoded_token = decode_token(token)
+      user_id = decoded_token.get('id')
+      if user := User.query.filter_by(id=user_id).first():
+        decoded_key = decoded_token.get('key')
+        current_key = current_app.config.get('SECRET_KEY') + user.password
+        if bcrypt.checkpw(current_key.encode('utf8'), decoded_key.encode('utf8')):
+          return { 'token': generate_token(user) }
+
+        return create_error('Password changed!', 401)
+      return create_error('User not found!', 404)
+    except jwt.ExpiredSignatureError:
+      return create_error('Signature expired. Please log in again.', 401)
+    except jwt.InvalidTokenError:
+      return create_error('Invalid token. Please log in again.', 400)
+
+  return create_error('No refresh token specified!', 404)
--- banck/utils.py
+++ banck/utils.py
@@ -1,4 +1,4 @@
-import datetime, jwt
+import datetime, jwt, bcrypt
 from flask import current_app
 
 
@@ -10,12 +10,31 @@ def create_error(message, status):
   }
 
 
+def generate_key(password):
+  key = current_app.config.get('SECRET_KEY') + password
+  return bcrypt.hashpw(key.encode('utf8'), bcrypt.gensalt()).decode('utf8')
+
+
 def generate_token(user):
   payload = {
     'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5),
     'iat': datetime.datetime.utcnow(),
     'id': user.id,
     'admin': user.admin
+  }
+
+  return jwt.encode(
+    payload,
+    current_app.config.get('SECRET_KEY'),
+    algorithm='HS256'
+  )
+
+
+def generate_refresh_token(user):
+  payload = {
+    'iat': datetime.datetime.utcnow(),
+    'id': user.id,
+    'key': generate_key(user.password)
   }
 
   return jwt.encode(
